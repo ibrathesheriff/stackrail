a03b9b012b69918a876343da1a375933e703ccff
ibrathesheriff <ibrasururu@gmail.com>
2025-06-12 16:52:04 +0200

    configuration reader file

---
+++ config.js
@@ -0,0 +1,11 @@
+// to get package.json
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const packageJson = JSON.parse(readFileSync(join(__dirname, 'package.json'), 'utf8'));
+
+export const STACKRAIL_VERSION = packageJson.version;
